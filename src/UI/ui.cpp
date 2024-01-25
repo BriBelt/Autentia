e74b53a7b00abdb48feb7184dfde09563ee96235
javiburn <jsarabia98@gmail.com>
2024-01-25 13:00:56 +0100

    score done

--- src/UI/ui.cpp
+++ src/UI/ui.cpp
@@ -1,10 +1,35 @@
 #include "../../includes/UI_hpp/ui.hpp"
 
+void	setPlayerIndex(int i)
+{
+	std::cout << "|";
+	std::cout << std::setw(1);
+	std::cout << i + 1;
+	std::cout << "|";
+}
+
+void	setPlayerScore(int num)
+{
+	for (int i = 0; i < 10; i++){
+		std::cout << std::setw(5);
+		std::cout << num;
+		std::cout << "|";
+	}
+}
+
+void	setPlayerName(std::string name)
+{
+	std::cout << std::setw(10);
+	std::cout << name;
+	std::cout << "|";
+}
+
+
 void	printHeader(void)
 {
-	std::cout << " __________________________________________________________________________ " << std::endl;
+	std::cout << " _________________________________________________________________________ " << std::endl;
 	std::cout << "|";
-	std::cout << std::setw(13);
+	std::cout << std::setw(12);
 	std::cout << "Names";
 	std::cout << "|";
 	for (int i = 0; i < 10; i++){
@@ -12,16 +37,24 @@ void	printHeader(void)
 		std::cout << i + 1;
 		std::cout << "|";
 	}
-	std::cout <<  std::endl;
-	std::cout << " __________________________________________________________________________ " << std::endl;
+	std::cout <<   std::endl;
+	std::cout << " _________________________________________________________________________ " << std::endl;
 }
 
 void	Printing::printScores(void)
 {
-	//int numberPlayers = 3;
+	int numberPlayers = 5;
+	std::string name = "JUAN";
 
 	printHeader();
-	/*for (int i = 0; i < numberPlayers; i++{
-
-	})*/
+	for (int i = 0; i < numberPlayers; i++){
+		setPlayerIndex(i);
+		setPlayerName(name);
+		setPlayerScore(i);
+		std::cout << std::endl;
+		/*std::cout << std::setw(1);
+		std::cout << i;
+		std::cout << "|";*/
+		std::cout << " _________________________________________________________________________ " << std::endl;
+	}
 }
