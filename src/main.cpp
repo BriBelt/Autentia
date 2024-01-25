3a32ebf3b6a96b04f8f08e88e4c142be2e9dc242
Federico Salazar Lois <fsalazar@c3r9s3.42madrid.com>
2024-01-25 12:01:19 +0100

    tree structure

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,15 @@
+#include "includes/include_1.hpp"
+
 int main()
 {
+	std::string str;
+
+	std::cout << "Welcome to our Bowling!" << std::endl;
+	std::cout << "Enter the number of players (Select a number between 1 and 4): ";
+	std::cin >> str;
+	if (std::atoi(str.c_str()) < 1 || std::atoi(str.c_str()) > 4)
+		return 0;
+	std::cout << str << std::endl;
+
 	return 0;
 }
