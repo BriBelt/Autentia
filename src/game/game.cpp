c3e4b4d8511f80671f41587d73cf2a0415cb6030
Federico Salazar Lois <fsalazar@c3r9s3.42madrid.com>
2024-01-25 12:27:58 +0100

    updated main

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -1,7 +1,7 @@
-#include "Game.hpp"
+#include "includes/game_hpp/game.hpp"
 #include <iostream>
 
-Game::Game(const std::string& name) : gameName(name), currentFrame(1) {
+Game::Game(const std::string& name, int numPlayers) : gameName(name), numPlayers(numPlayers), currentFrame(1) {
     // Initialization code, if any
 }
 
@@ -28,6 +28,36 @@ void Game::nextFrame() {
 }
 
 void Game::playTurn() {
-    // Add logic for handling player turns, scoring, etc.
-    // You may want to interact with the Player and Calculation classes here.
+    std::cout << "Round " << currentFrame << " - Enter scores for each player:" << std::endl;
+
+    for (const auto& player : playersMap) {
+        std::string playerName = player.first;
+        int score;
+
+        std::cout << "Enter score for player " << playerName << ": ";
+        std::cin >> score;
+
+        // Add logic to update player scores, handle strikes, spares, etc.
+        // You may want to interact with the Player and Calculation classes here.
+    }
+
+    // After all players have entered their scores, you can print the results or perform additional logic.
+}
+
+
+// Getters and setters
+std::string Game::getName() const {
+    return gameName;
+}
+
+int Game::getCurrentFrame() const {
+    return currentFrame;
+}
+
+void Game::setName(const std::string& name) {
+    gameName = name;
+}
+
+void Game::setCurrentFrame(int frame) {
+    currentFrame = frame;
 }
