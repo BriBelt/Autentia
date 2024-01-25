83772ce43b9fa94b187e491f30c98d3f99149b45
Fabricio <flopez-r@student.42madrid.com>
2024-01-25 13:28:51 +0100

    Class ParseInput added to checkers part

--- includes/checkers_hpp/checker.hpp
+++ includes/checkers_hpp/checker.hpp
@@ -5,4 +5,16 @@
 # include <iostream>
 # include <string>
 
+class ParseInput
+{
+  public:
+	bool check_num_players(std::string num_players_str);
+	bool check_name_players(std::string &name);
+	bool check_correct_round(std::string a_str, std::string b_str);
+	bool check_correct_shoot(std::string a_str);
+
+  private:
+	/* data */
+};
+
 #endif
--- src/checkers/checkers.cpp
+++ src/checkers/checkers.cpp
@@ -1,42 +1,70 @@
 #include "checker.hpp"
 
 //Function checks number of players must be less or equal than 4
-bool    check_num_players(int   num_players)
+bool ParseInput::check_num_players(std::string num_players_str)
 {
-    if (!isdigit(num_players))
-        return (false);
-    if (num_players < 4)
-        return (false);
-    return (true);
+	int	num_players;
+
+	try
+	{
+		num_players = std::stoi(num_players_str);
+		if (num_players < 0 || num_players > 4)
+			return (false);
+		return (true);
+	}
+	catch (const std::invalid_argument &)
+	{
+		return (false);
+	}
 }
 
 //Function Verify if the string is printable and less than 10 characters
-bool    check_name_players(std::string &name)
+bool ParseInput::check_name_players(std::string &name)
 {
-    if (name.size() > 10)
-        return (false);
-    for (size_t i = 0; i < name.size(); i++)
-    {
-        if (!isprint(name[i]))
-            return (false);
-        i++;    
-    }
-    return (true);
+	if (name.size() > 10)
+		return (false);
+	for (size_t i = 0; i < name.size(); i++)
+	{
+		if (!isprint(name[i]))
+			return (false);
+		i++;
+	}
+	return (true);
 }
 
 //Function check if the round has a correct score (a + b <= 10)
-bool    check_correct_round(int a, int b)
+bool ParseInput::check_correct_round(std::string a_str, std::string b_str)
 {
-    if ((a + b > 10) && (a >= 0 && b >= 0))
-        return (false);
-    return (true);
+	int	a;
+	int	b;
+	try
+	{
+		a = std::stoi(a_str);
+		b = std::stoi(b_str);
+		if ((a + b > 10) && (a < 0 && b < 0))
+			return (false);
+		return (true);
+	}
+	catch(const std::invalid_argument &)
+	{
+		return (false);
+	}
 }
 
 //Function check if the shoot has a correct score (a >= 0 && a <= 10)
-bool    check_correct_shoot(int a)
+bool ParseInput::check_correct_shoot(std::string a_str)
 {
-    if (!isnumber(a) && (a < 0 && a > 10))
-        return (false);
-    return (true);
-}
+	int	a;
 
+	try
+	{
+		a = std::stoi(a_str);
+		if (!isnumber(a) && (a < 0 && a > 10))
+			return (false);
+		return (true);
+	}
+	catch (const std::invalid_argument &)
+	{
+		return (false);
+	}
+}
